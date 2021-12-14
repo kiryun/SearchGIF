e12fbba769b263b66badf0035bcbd8e01536a495
Wimes <wimes@Wimesui-MacBookPro.local>
2021-12-14 22:27:49 +0900

    add dependency, without storyboard

---
+++ SearchGIF/Scene/Search/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  SearchGIF
+//
+//  Created by Wimes on 2021/12/14.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .purple
+    }a
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
